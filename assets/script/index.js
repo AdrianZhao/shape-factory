846e4c2f9d5be381a37f60c60d2702c49d2092cd
Adrian Zhao <achnnal@gmail.com>
2023-03-14 19:31:32 -0500

    js

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,3 +4,77 @@
   Yuhan Zhao
 */
 
+class DivBox {
+  #shape;
+  #color;
+  constructor(shape, color) {
+    this.#shape = shape;
+    this.#color = color;
+  }
+  set shape(shape) { this.#shape = shape; }
+  set color(color) { this.#color = color; }
+  get shape() { return this.#shape; } 
+  get color() { return this.#color; }
+  getInfo() {
+    return `${this.#shape} ${this.#color}`;
+  }
+}
+
+const shape = document.querySelector('#shape');
+const color = document.querySelector('#color');
+const message = document.querySelector('.message');
+const button = document.querySelector('button');
+const generate = document.querySelector('.generate');
+
+function getShape() {
+  return shape.value;
+}
+function getColor() {
+  return color.value;
+}
+function getInformation() {
+  return `${div.value}`;
+}
+function setShape(div) {
+  let temp = getShape();
+  if (temp == 'Circle') {
+    div.style.borderRadius = '50%';
+  } else {
+    div.style.borderRadius = '5px';
+  }
+}
+function setColor(div) {
+  let temp = getColor();
+  if (temp == 'Blue') {
+    div.style.backgroundColor = '#09f';
+  } else if (temp == 'Green') {
+    div.style.backgroundColor = '#9f0';
+  } else if (temp == 'Orange') {
+    div.style.backgroundColor = '#f90';
+  } else if (temp == 'Pink') {
+    div.style.backgroundColor = '#f09';
+  } else {
+    div.style.backgroundColor = '#90f';
+  }
+}
+function addDiv() {
+  const divs = document.createElement("div");
+  divs.id = number;
+  divs.value = getShape() + ' ' + getColor();
+  setShape(divs);
+  setColor(divs);
+  generate.appendChild(divs); 
+  divs.addEventListener('click', () => {
+    message.innerText = 'Unit ' + divs.id + ' ' + divs.value;
+  })
+}
+let number = 0;
+button.addEventListener('click', () => {
+  if(number < 20) {
+    number++;
+    addDiv();
+  } else {
+    message.innerText = `The Box is Full!`;
+  }
+})
+
